2a385b17c488a8109ab0fa8b1ba94bff431f7d4e
Harshil Patel <harshil.patel@techintact.com>
2026-03-08 01:50:14 +0530

    [added express file uploads]

--- backend/routers/product.router.js
+++ backend/routers/product.router.js
@@ -4,13 +4,14 @@ import {
   getProducts,
   updateProduct,
 } from "../controllers/ProductController";
+import fileUpload from "express-fileupload";
 
 const router = express.Router();
 
 // actual router instance which interacts with the request and response cycle
 
 router.get(`/get`, getProducts);
-router.post(`/add`, createProduct);
-router.post(`/edit/:id`, updateProduct);
+router.post(`/add`, fileUpload(), createProduct);
+router.post(`/edit/:id`, fileUpload(), updateProduct);
 
 export default router;
