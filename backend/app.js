59c1662d4735cb152d697f13425a7e9be65dec16
Harshil Patel <harshil.patel@techintact.com>
2026-03-08 02:24:11 +0530

    [added the products on client side]

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,15 @@ import cors from "cors";
 import authRoutes from "./routers/authRoutes.js";
 import productRouter from "./routers/product.router.js";
 import dotenv from "dotenv";
+import path from "path";
 
 const app = express();
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(`/uploads`, express.static(path.join(process.cwd(), "public")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRouter);
--- frontend/src/Pages/Products.jsx
+++ frontend/src/Pages/Products.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import products from '../Data/product'
-import Card from '../Components/card'
-import { useCart } from '../Context/CartContext'
-
+import React, { useEffect } from "react";
+// import products from "../Data/product";
+import Card from "../Components/card";
+import { useCart } from "../Context/CartContext";
+import axios from "axios";
+import { useState } from "react";
 
 const Products = () => {
-  
+  const [products, setProducts] = useState([]);
+  const fecthProducts = async () => {
+    try {
+      const res = await axios.get("http://localhost:8000/api/products/get");
+
+      console.log("res", res);
+      return setProducts(res.data);
+    } catch (error) {
+      console.log("got runtime exception from the fecthProducts route handler");
+    }
+  };
+  useEffect(() => {
+    fecthProducts();
+  }, []);
+
+  // console.log("products", products);
   return (
     <div className="w-full grid grid-cols-4 gap-6 p-10">
       {products.map((product) => (
         <Card key={product.id} product={product} />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
